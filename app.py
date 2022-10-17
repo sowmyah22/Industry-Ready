299f0b6ccfb4c058e17f6570e0a781f97f527a86
sowmyah22 <sowmyah871@gmail.com>
2022-10-18 00:40:47 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app=Flask( __name__)
 
 @app.route('/',methods=['GET'])
 def hello():
-    return 'Helo World'
+    return 'Helo World!!Its My First Application Of Dockers'
 
 if __name__=='__main__':
     app.run(host="0.0.0.0",port=5000)
